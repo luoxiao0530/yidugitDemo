d6a622689e1a7c63f09aca0e7123bb4c4d22f934
LvMuYing <3430163455@qq.com>
2020-12-25 15:00:20 +0800

    User

---
+++ src/com/yidu/User.java
@@ -0,0 +1,13 @@
+package com.yidu;
+
+/**
+ * @description:
+ * @author: LvFang
+ * date:2020/12/25 14:59
+ * version:1.0
+ */
+public class User {
+    public static void main(String[] args) {
+        System.out.println("User");
+    }
+}

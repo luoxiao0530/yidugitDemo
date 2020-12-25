a2835860d135b80008e81e1611fb08aeeea0a6d3
86177 <CPHbusters0530>
2020-12-25 14:47:37 +0800

    这是第一次

---
+++ src/com/yidu/yidu.java
@@ -0,0 +1,13 @@
+package com.yidu;
+
+/**
+ * @author
+ * @version 1.0
+ * @description:
+ * @adte 2020/12/25 14:44
+ */
+public class yidu {
+    public static void main(String[] args) {
+        System.out.println("一度教育");
+    }
+}
